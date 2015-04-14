18606be49d5cc8e803384591c49f9b2d2ab8ef92
chunfuchang <chunfuchang@potix.com>
2015-04-14 08:34:13 +0100

    setWidth/Height, setHflex/Vflex is done (hopefully....)

--- src/main/resources/web/js/tbeditor/Tbeditor.js
+++ src/main/resources/web/js/tbeditor/Tbeditor.js
@@ -1,55 +1,16 @@
-/**
- *
- * Base naming rule:
- * The stuff start with "_" means private , end with "_" means protect ,
- * others mean public.
- *
- * All the member field should be private.
- *
- * Life cycle: (It's very important to know when we bind the event)
- * A widget will do this by order :
- * 1. $init
- * 2. set attributes (setters)
- * 3. rendering mold (@see mold/tbeditor.js )
- * 4. call bind_ to bind the event to dom .
- *
- * this.deskop will be assigned after super bind_ is called,
- * so we use it to determine whether we need to update view
- * manually in setter or not.
- * If this.desktop exist , means it's after mold rendering.
- *
- */
 tbeditor.Tbeditor = zk.$extends(zul.Widget, {
 	_value: '', //default value for text attribute
 	_height: '',
 	_cnt: '',
+	_jqCnt: '',
 	_config: {},
 	_beginChangeValue: 0,
 	
 	$init: function () {
 		this.$supers('$init', arguments);
-		
 	},
-	/**
-	 * Don't use array/object as a member field, it's a restriction for ZK object,
-	 * it will work like a static , share with all the same Widget class instance.
-	 *
-	 * if you really need this , assign it in bind_ method to prevent any trouble.
-	 *
-	 * TODO:check array or object , must be one of them ...I forgot. -_- by Tony
-	 */
 	
 	$define: {
-		/**
-		 * The member in $define means that it has its own setter/getter.
-		 * (It's a coding sugar.)
-		 *
-		 * If you don't get this ,
-		 * you could see the comment below for another way to do this.
-		 *
-		 * It's more clear.
-		 *
-		 */
 		value: function() { //this function will be called after setText() .
 		
 			if (this.desktop) {
@@ -73,8 +34,10 @@ tbeditor.Tbeditor = zk.$extends(zul.Widget, {
 		if (!value) return;
 		
 		if (prop == 'height') {
-			jq(this.$n('cnt')).css({minHeight: value});
-			jq(this.$n('cnt')).parent().css({minHeight: value});
+			//set editor's height as parent div
+			var contentHeight = this._calcEditorHeight(value.endsWith('%') ? jq(this.$n()).outerHeight() : value);
+			jq(this.$n('cnt')).css({minHeight: contentHeight});
+			jq(this.$n('cnt')).parent().css({minHeight: contentHeight});
 		} else {
 			jq(this.$n()).width(value);
 		}
@@ -99,6 +62,7 @@ tbeditor.Tbeditor = zk.$extends(zul.Widget, {
 		this.$supers(tbeditor.Tbeditor,'bind_', arguments);
 		
 		this._cnt = this.$n('cnt');
+		this._jqCnt = jq(this._cnt);
 		var wgt = this;
 		jq(this._cnt).trumbowyg(this._config)
 					.on('tbwfocus', function(evt) { wgt.proxy(wgt.doFocus_)(jq.Event.zk(evt, wgt)); })
@@ -107,22 +71,11 @@ tbeditor.Tbeditor = zk.$extends(zul.Widget, {
 					.on('tbwpaste', function() { wgt._onPaste(wgt); })
 					.on('tbwclose', function() { wgt._onClose(wgt); });
 		
-		/*if (this._height) {
-			jq(this.$n('cnt')).css({minHeight: this._height});
-			jq(this.$n('cnt')).parent().css({minHeight: this._height});
-		}*/
 		zWatch.listen({
 			onSize : this
 		});
-			
-	
-		//A example for domListen_ , REMEMBER to do domUnlisten in unbind_.
-		//this.domListen_(this.$n("cave"), "onClick", "_doItemsClick");
 	},
 	unbind_: function () {
-	
-		// A example for domUnlisten_ , should be paired with bind_
-		// this.domUnlisten_(this.$n("cave"), "onClick", "_doItemsClick");
 		jq(this._cnt).trumbowyg('destroy');
 		
 		zWatch.unlisten({
@@ -133,14 +86,19 @@ tbeditor.Tbeditor = zk.$extends(zul.Widget, {
 	},
 	setFlexSize_: function(sz, ignoreMargins) {
 		this.$supers('setFlexSize_', arguments);
+		//hflex isn't handled here
 		if (sz.height) {
-			var contentHeight = sz.height - zk.parseInt(jq(this.$n()).children().css('marginTop'))
-					- zk.parseInt(jq(this.$n()).children().css('marginBottom'))
-					- 2 * zk.parseInt(jq(this.$n()).children().css('border'))
-					- jq(this.$n()).find('ul').outerHeight();
+			var contentHeight = this._calcEditorHeight(sz.height);
 			jq(this.$n('cnt')).css({minHeight: contentHeight});
 			jq(this.$n('cnt')).parent().css({minHeight: contentHeight});
 		}
+	},
+	_calcEditorHeight: function(height) {
+		var tbBox = jq(this.$n()).children().eq(0);
+		return zk.parseInt(height) - zk.parseInt(tbBox.css('marginTop'))
+					- zk.parseInt(tbBox.css('marginBottom'))
+					- 2 * zk.parseInt(tbBox.css('border'))
+					- jq(this.$n()).find('ul').outerHeight(); //buttons' height
 	},
 	onSize: function() {
 		this.$supers('onSize', arguments);
@@ -153,23 +111,23 @@ tbeditor.Tbeditor = zk.$extends(zul.Widget, {
 	},
 	_onBlur: function(evt) {
 		//we take it as onChange
-		this._value = jq(this._cnt).trumbowyg('html');//evt.target.innerHTML;
+		this._value = jq(this._cnt).trumbowyg('html');//same with evt.target.innerHTML;
 		this.fire('onChange', {value: this._value});
 	},
 	_onChange: function(evt) {
 		//actually, it's onChanging
 		var formerText = this._value;
-		var laterText = jq(this._cnt).trumbowyg('html');//evt.target.innerHTML;
+		var laterText = jq(this._cnt).trumbowyg('html');//same with evt.target.innerHTML;
 		if (formerText != laterText) {
 			this._value = laterText;
 			this.fire('onChanging', {value: this._value});
 		}
 	},
 	_onPaste: function(wgt) {
-		console.log('paste');
+		//do nothing
 	},
 	_onClose: function(wgt) {
-		console.log('close');
+		//do nothing
 	},
 	getZclass: function () {
 		return this._zclass != null ? this._zclass: "z-tbeditor";
